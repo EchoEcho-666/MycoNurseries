eac93dde157c5e7f4cdb85c0d7c79bf72237e52b
Lin zengyu <zengyulin970@gmail.com>
2025-03-19 17:54:39 -0400

    update server.js in backend

--- my-backend/server.js
+++ my-backend/server.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const subscribeRouter = require('./routes/subscribe');
 
+// Parse JSON bodies
 app.use(express.json());
 
+// Serve static files from the React app build folder
+app.use(express.static(path.join(__dirname, '../my-frontend/build')));
+
 // Mount your subscription endpoint under '/subscribe'
-app.use('/subscribe', subscribeRouter);
+app.use('/subscribe', require('./routes/subscribe'));
+
+// Catch-all: for any request that doesn't match above, send 404.html
+app.get('*', (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, '../my-frontend/build', '404.html'));
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
