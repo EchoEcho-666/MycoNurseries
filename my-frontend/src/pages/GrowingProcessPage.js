8417af75ec765314a967a4bc75b79452d7ae0b8a
Lin zengyu <zengyulin970@gmail.com>
2025-09-28 21:48:00 -0400

    growing process page finialized update

--- my-frontend/src/pages/GrowingProcessPage.js
+++ my-frontend/src/pages/GrowingProcessPage.js
@@ -23,6 +23,25 @@ const Heading = styled.h2`
 const Paragraph = styled.p`
 `;
 
+const SectionTitle = styled.h2`
+  color: #39683B;
+  font-family: 'Montserrat', sans-serif;
+  font-size: 1.628rem;
+  font-weight: bold;
+  margin-bottom: 0.5em;
+  text-align: center;
+  position: relative;
+
+  &::after {
+    content: "";
+    display: block;
+    width: 100px;
+    height: 2px;
+    background-color: #A52A2A;
+    margin: 0.28em auto 0;
+  }
+`;
+
 function GrowingProcess() {
   const { t } = useTranslation();
 
@@ -30,13 +49,15 @@ function GrowingProcess() {
     <>
       <Container>
         <Heading>{t('components.growingProcess.title')}</Heading>
-        <Paragraph>{t('components.growingProcess.text')}</Paragraph>
-        
-        <Image src={process.env.PUBLIC_URL + "/assets/growing-process/flowchart.jpg"} alt="Mushroom Growing Flowchart" />
-        <Paragraph>{t('components.growingProcess.flowchartDesc')}</Paragraph>
-
-        <Image src={process.env.PUBLIC_URL + "/assets/growing-process/infographic.jpg"} alt="Mushroom Growing Infographic" />
-        <Paragraph>{t('components.growingProcess.infographicDesc')}</Paragraph>
+        <Paragraph>{t('components.growingProcess.text1')}</Paragraph>
+        <Paragraph>{t('components.growingProcess.text2')}</Paragraph>
+        <Image src={process.env.PUBLIC_URL + "/assets/growing-process/Myco Flow Chart.png"} alt="Mushroom Growing Flowchart" />
+        <SectionTitle>{t('components.growingProcess.sectionTitle1')}</SectionTitle>
+        <Paragraph>{t('components.growingProcess.text3')}</Paragraph>
+        <Paragraph>{t('components.growingProcess.text4')}</Paragraph>
+        <Image src={process.env.PUBLIC_URL + "/assets/growing-process/group.webp"} alt="Group Picture" />
+        <Image src={process.env.PUBLIC_URL + "/assets/growing-process/growBag.webp"} alt="Grow Bag" />
+        <Image src={process.env.PUBLIC_URL + "/assets/growing-process/mushroom.webp"} alt="Mushroom" />      
       </Container>
     </>
   );
