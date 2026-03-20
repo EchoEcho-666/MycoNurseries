8a28090b9649055810ca3cc1ac60d256fd64dd54
hutchallison <aahutchings06@gmail.com>
2026-03-20 17:00:06 -0400

    made blog page prettier

--- my-frontend/src/pages/Blog.js
+++ my-frontend/src/pages/Blog.js
@@ -26,10 +26,11 @@ const BlogTitle = styled.h2`
   }
 `;
 
-// const CardContainer = styled.div`
-//   text-align: left;
-//   margin-left: -15px;
-// `;
+const CardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
 
 function Blog() {
   const { t } = useTranslation();
@@ -41,11 +42,11 @@ function Blog() {
         <BlogTitle>{t('components.blog.title', 'Blog')}</BlogTitle>
         <p>{t('components.blog.description', 'Welcome to our blog. Here you are our latest updates and articles.')}</p>
         <br></br>
-        <div className = "cardArray">
+        <CardContainer>
           {posts.map((post) => (
             <BlogSnapshot key={post.slug} post={post} />
           ))}
-        </div>
+        </CardContainer>
       </BlogWrapper>
     </>
   );
