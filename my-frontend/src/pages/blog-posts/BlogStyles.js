60c22331be105041c8c9c1479623fa04fc9909c6
hutchallison <aahutchings06@gmail.com>
2026-03-20 22:52:44 -0400

    updated blog 1

--- my-frontend/src/pages/blog-posts/BlogStyles.js
+++ my-frontend/src/pages/blog-posts/BlogStyles.js
@@ -9,7 +9,10 @@ export const BlogWrapper = styled.section`
   text-align: center;
 `;
 
-export const TitleHeading = styled.h1``;
+export const TitleHeading = styled.h1`
+  color: #54452f;
+  
+`;
 export const SubHeading = styled.h2``;
 export const AuthorHeading = styled.h3``;
 
@@ -25,7 +28,7 @@ export const BodyRow = styled.div`
   align-items: center;
   justify-content: center;
   flex-wrap: wrap;
-  gap: 2em;
+  gap: 0.5em;
   flex-direction: ${({ reverse }) => (reverse ? 'row-reverse' : 'row')};
   margin: 2em 0;
 `;
@@ -51,10 +54,38 @@ export const ImageColumn = styled.div`
 `;
 
 export const ParagraphGroup = styled.div`
-  max-width: 70%;
+  max-width: 85%;
 `;
 
 export const Paragraph = styled.p`
   text-align: left;
   margin-bottom: 1em;
+`;
+
+export const Reference = styled.p`
+  padding-left: 3em;
+  text-indent: -3em;
+  text-align: left;
+  margin-bottom: 1em;
+`;
+
+export const ReferenceHeading = styled.div`
+  text-align: center;
+  margin-bottom: 4em;
+`;
+
+export const Bar = styled.div`
+  width: 75px;
+  height: 2px;
+  background-color: #54452f;
+  margin: 10px auto;
+  margin-top: 1em;
+`;
+
+export const ImageCaption = styled.span`
+  display: block;
+  font-size: 0.9rem;
+  text-align: center;
+  margin-top: 0.3em;
+  font-style: italic;
 `;
